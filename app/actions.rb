d185ed56895c435b9bb19713d1127f16b0470cff
climbing code <t-j-davis@hotmail.com>
2014-07-30 13:56:58 -0700

    routes

--- app/actions.rb
+++ app/actions.rb
@@ -1,14 +1,36 @@
 # Homepage (Root path)
+
 get '/' do
-	hexcolor = Hexcolor.first 
-	@hexcolor = hexcolor.hextriplet 
+	@session = nil
+	erb :index
+end
+
+get '/:id' do
+	@session = Account.find_by(params[:id].to_i)
+	hex = @session.hexcolors.where(account_id: @session.id)
+	@hexcolor = hex.pluck(:hextriplet)
   erb :index
 end
 
-get '/account' do
+get '/account/:id' do
+	
 	erb :'account/index'
 end
 
 post '/signin' do
+	@account = Account.where( params[:username] ).where ( params[:password] )
+	if @account != nil
+		redirect "account/#{params[@account.id]}"
+	else
+		raise "This is not an account"
+	end
+end
 
+
+
+
+post '/dashboard/:id' do
+
+
+	redirect "/#{params[:id]}"
 end
