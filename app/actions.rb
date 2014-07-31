68e0dbce3beabcc71d54b93eb12ed6655ea55104
climbing code <t-j-davis@hotmail.com>
2014-07-31 01:33:23 -0700

    uploads are working

--- app/actions.rb
+++ app/actions.rb
@@ -44,27 +44,33 @@ end
 
 post '/account/:id' do
 
-	@brand = Account.find(params[:id])
-	@uploaded_file = @brand.uploads.create :file => params[:upload1]
-	@brand.save!
+	@brand1 = Account.find(params[:id])
+	@uploaded_file = @brand1.uploads.create :file => params[:upload1]
+	@brand1.save!
 
-	"<pre>#{@uploaded_file.to_s}</pre>"
+	@brand2 = Account.find(params[:id])
+	@uploaded_file = @brand2.uploads.create :file => params[:upload2]
+	@brand2.save!
 
-	@file1 = File.open(File.join(APP_ROOT, '/uploads', params['upload1'][:filename]), "w") do |f|
-		f.write(params['upload1'][:tempfile].read)
-	end
+	@brand3 = Account.find(params[:id])
+	@uploaded_file = @brand3.uploads.create :file => params[:upload3]
+	@brand3.save!
+
+	# @file1 = File.open(File.join(APP_ROOT, '/uploads', params['upload1'][:filename]), "w") do |f|
+	# 	f.write(params['upload1'][:tempfile].read)
+	# end
 	
 	@hexcolor1 = Hexcolor.new(
 		hextriplet: params[:hexcolor1],
-		account_id: @brand.id,
+		account_id: params[:id]
 		)
 	@hexcolor2 = Hexcolor.new(
 		hextriplet: params[:hexcolor2],
-		account_id: @brand.id
+		account_id: params[:id]
 		)
 	@hexcolor3 = Hexcolor.new(
 		hextriplet: params[:hexcolor3],
-		account_id: @brand.id
+		account_id: params[:id]
 		)
 
 	if (@hexcolor1.save || @hexcolor1.hextriplet == nil) && (@hexcolor2.save || @hexcolor2.hextriplet == nil) && (@hexcolor3.save || @hexcolor3.hextriplet == nil)
